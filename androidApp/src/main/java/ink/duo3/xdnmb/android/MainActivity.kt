e5b34d81ce404da6ecdb6a3ed1f2e2fcd79ca158
KonYaco <atwzj233@gmail.com>
2022-10-30 20:22:53 +0800

    Update

--- androidApp/src/main/java/ink/duo3/xdnmb/android/MainActivity.kt
+++ androidApp/src/main/java/ink/duo3/xdnmb/android/MainActivity.kt
@@ -4,35 +4,43 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import ink.duo3.xdnmb.shared.XdSDK
 import ink.duo3.xdnmb.android.ui.ForumsDisplay
 import ink.duo3.xdnmb.android.ui.theme.AppTheme
+import ink.duo3.xdnmb.shared.XdSDK
 import ink.duo3.xdnmb.shared.data.cache.DatabaseDriverFactory
 import ink.duo3.xdnmb.shared.data.entity.ForumGroup
 
 class MainActivity : ComponentActivity() {
+    private var forumList by mutableStateOf<List<ForumGroup>?>(null)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            LaunchedEffect(Unit) {
+                init()
+            }
+
             AppTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    ForumsDisplay(forumList = getForums())
+                    ForumsDisplay(forumList = forumList)
                 }
             }
         }
     }
+
     private val sdk = XdSDK(DatabaseDriverFactory(this))
 
-    fun getForums(): List<ForumGroup>? {
-        var forumList: List<ForumGroup>? = null
-        suspend {
-            forumList = sdk.getForumList(true)
-        }
-        return forumList
+    private suspend fun init() {
+        forumList = sdk.getForumList(true)
     }
 }
--- shared/src/commonMain/kotlin/ink/duo3/xdnmb/shared/XdSDK.kt
+++ shared/src/commonMain/kotlin/ink/duo3/xdnmb/shared/XdSDK.kt
@@ -4,21 +4,24 @@ import ink.duo3.xdnmb.shared.data.cache.Database
 import ink.duo3.xdnmb.shared.data.cache.DatabaseDriverFactory
 import ink.duo3.xdnmb.shared.data.entity.ForumGroup
 import ink.duo3.xdnmb.shared.network.XdApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class XdSDK(databaseDriverFactory: DatabaseDriverFactory) {
     private val database = Database(databaseDriverFactory)
     private val api = XdApi()
 
     @Throws(Exception::class)
-    suspend fun getForumList(forceReload: Boolean): List<ForumGroup> {
-        val cachedForumList = database.getAllForums()
-        return if (cachedForumList.isNotEmpty() && !forceReload) {
-            cachedForumList
-        } else {
-            api.getForumList().also {
-                database.clearDatabase()
-                database.createForumList(it)
+    suspend fun getForumList(forceReload: Boolean): List<ForumGroup> =
+        withContext(Dispatchers.Default) {
+            val cachedForumList = database.getAllForums()
+            return@withContext if (cachedForumList.isNotEmpty() && !forceReload) {
+                cachedForumList
+            } else {
+                api.getForumList().also {
+                    database.clearDatabase()
+                    database.createForumList(it)
+                }
             }
         }
-    }
 }
