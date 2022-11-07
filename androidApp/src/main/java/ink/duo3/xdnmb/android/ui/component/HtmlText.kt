e98deb02c441f59b7fee70a24a6811837eaf3b2b
KonYaco <atwzj233@gmail.com>
2022-11-07 10:00:35 +0800

    Update HtmlText

--- androidApp/src/main/java/ink/duo3/xdnmb/android/ui/component/HtmlText.kt
+++ androidApp/src/main/java/ink/duo3/xdnmb/android/ui/component/HtmlText.kt
@@ -1,23 +1,41 @@
 package ink.duo3.xdnmb.android.ui.component
 
+import android.graphics.Typeface
+import android.text.Spannable
+import android.text.style.CharacterStyle
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import android.text.style.URLSpan
+import android.text.style.UnderlineSpan
+import android.util.TypedValue
 import android.widget.TextView
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
+import androidx.core.text.toSpannable
 import ink.duo3.xdnmb.android.ui.theme.AppTheme
 
 @Composable
 fun HtmlText(
-    html: String
+    html: String,
+    style: TextStyle = LocalTextStyle.current
 ) {
-    AndroidView(
-        factory = {
-            TextView(it)
-        }
-    ) {
-        it.text = HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_COMPACT)
-    }
+    HtmlComposeText(html, style)
 }
 
 
@@ -25,8 +43,160 @@ fun HtmlText(
 @Composable
 private fun Preview() {
     AppTheme {
-        HtmlText(html = """
-            <span style="color: red">Hello, <i>world</i></span>
-        """.trimIndent())
+        Surface(color = MaterialTheme.colorScheme.background) {
+            HtmlText(
+                html = """
+            Hello, <span style="color: red"><i>world</i></span>
+        """.trimIndent(),
+                style = LocalTextStyle.current
+            )
+        }
     }
+}
+
+@Composable
+private fun HtmlTextView(
+    html: String,
+    style: TextStyle = LocalTextStyle.current
+) {
+    AndroidView(
+        factory = {
+            TextView(it)
+        }
+    ) {
+        it.text = HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_COMPACT)
+        it.setTextColor(style.color.value.toInt())
+        it.setTextSize(TypedValue.COMPLEX_UNIT_SP, style.fontSize.value)
+        it.letterSpacing = style.letterSpacing.value
+    }
+}
+
+@Composable
+private fun HtmlComposeText(
+    html: String,
+    style: TextStyle
+) {
+    Text(text = buildAnnotatedStringFromHtml(html), style = style)
+}
+
+@Composable
+fun buildAnnotatedStringFromHtml(html: String): AnnotatedString {
+    val text = remember(html) { HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_COMPACT) }
+    val color = LocalContentColor.current
+    return remember(html) {
+        text.toSpannable().toAnnotatedString(color)
+    }
+}
+
+
+fun Spannable.toAnnotatedString(primaryColor: Color): AnnotatedString {
+    val builder = AnnotatedString.Builder(this.toString())
+    val copierContext = CopierContext(primaryColor)
+    SpanCopier.values().forEach { copier ->
+        getSpans(0, length, copier.spanClass).forEach { span ->
+            copier.copySpan(span, getSpanStart(span), getSpanEnd(span), builder, copierContext)
+        }
+    }
+    return builder.toAnnotatedString()
+}
+
+private data class CopierContext(
+    val primaryColor: Color,
+)
+
+private enum class SpanCopier {
+    URL {
+        override val spanClass = URLSpan::class.java
+        override fun copySpan(
+            span: Any,
+            start: Int,
+            end: Int,
+            destination: AnnotatedString.Builder,
+            context: CopierContext
+        ) {
+            val urlSpan = span as URLSpan
+            destination.addStringAnnotation(
+                tag = name,
+                annotation = urlSpan.url,
+                start = start,
+                end = end,
+            )
+            destination.addStyle(
+                style = SpanStyle(
+                    color = context.primaryColor,
+                    textDecoration = TextDecoration.Underline
+                ),
+                start = start,
+                end = end,
+            )
+        }
+    },
+    FOREGROUND_COLOR {
+        override val spanClass = ForegroundColorSpan::class.java
+        override fun copySpan(
+            span: Any,
+            start: Int,
+            end: Int,
+            destination: AnnotatedString.Builder,
+            context: CopierContext
+        ) {
+            val colorSpan = span as ForegroundColorSpan
+            destination.addStyle(
+                style = SpanStyle(color = Color(colorSpan.foregroundColor)),
+                start = start,
+                end = end,
+            )
+        }
+    },
+    UNDERLINE {
+        override val spanClass = UnderlineSpan::class.java
+        override fun copySpan(
+            span: Any,
+            start: Int,
+            end: Int,
+            destination: AnnotatedString.Builder,
+            context: CopierContext
+        ) {
+            destination.addStyle(
+                style = SpanStyle(textDecoration = TextDecoration.Underline),
+                start = start,
+                end = end,
+            )
+        }
+    },
+    STYLE {
+        override val spanClass = StyleSpan::class.java
+        override fun copySpan(
+            span: Any,
+            start: Int,
+            end: Int,
+            destination: AnnotatedString.Builder,
+            context: CopierContext
+        ) {
+            val styleSpan = span as StyleSpan
+
+            destination.addStyle(
+                style = when (styleSpan.style) {
+                    Typeface.ITALIC -> SpanStyle(fontStyle = FontStyle.Italic)
+                    Typeface.BOLD -> SpanStyle(fontWeight = FontWeight.Bold)
+                    Typeface.BOLD_ITALIC -> SpanStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontStyle = FontStyle.Italic
+                    )
+                    else -> SpanStyle()
+                },
+                start = start,
+                end = end,
+            )
+        }
+    };
+
+    abstract val spanClass: Class<out CharacterStyle>
+    abstract fun copySpan(
+        span: Any,
+        start: Int,
+        end: Int,
+        destination: AnnotatedString.Builder,
+        context: CopierContext
+    )
 }
