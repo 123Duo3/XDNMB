2ad2fc74b91ea92f72e793d83971971a6de26b4b
KonYaco <atwzj233@gmail.com>
2022-11-07 09:45:31 +0800

    Update test

--- shared/src/commonTest/kotlin/ink/duo3/xdnmb/shared/network/XdApiTest.kt
+++ shared/src/commonTest/kotlin/ink/duo3/xdnmb/shared/network/XdApiTest.kt
@@ -1,12 +1,34 @@
 package ink.duo3.xdnmb.shared.network
 
 import kotlinx.coroutines.runBlocking
+import kotlin.test.BeforeTest
 import kotlin.test.Test
 
 internal class XdApiTest {
+    private lateinit var xdApi: XdApi
+
+    @BeforeTest
+    fun setup() {
+        xdApi = XdApi()
+    }
+
+    @Test
+    fun testGetTimeline() = runBlocking {
+        xdApi.getTimeLine(1).forEach {
+            println(it)
+        }
+    }
+
     @Test
     fun testGetForumList() = runBlocking {
-        XdApi().getTimeLine(1).forEach {
+        xdApi.getForumList().forEach {
+            println(it)
+        }
+    }
+
+    @Test
+    fun testGetThreadList() = runBlocking {
+        xdApi.getTreadList("14", 1).forEach {
             println(it)
         }
     }
